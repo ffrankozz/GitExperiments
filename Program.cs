6ff3dc170e98d5cadc5d619e933752b7d5e34e16
FFrankOzz <37096255+ffrankozz@users.noreply.github.com>
2022-07-24 14:16:06 +0300

    Added the third line in program.cs line indication we are in the CommandLine fodler project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Learning Git with simple experiments!");
+Console.WriteLine("Learning Git with simple experiments! Add this line in VS folder project  and then pushed");
+Console.WriteLine("  Added this line in CommandLine project(via VS)   and then pushed");
