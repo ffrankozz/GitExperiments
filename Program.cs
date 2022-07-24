f1daf79928520da5bd8efa37f3fe96cf8b90d581
FFrankOzz <37096255+ffrankozz@users.noreply.github.com>
2022-07-24 15:26:12 +0300

    New message added to Program.cs file to indicate this change has been added in the Commandline project

--- Program.cs
+++ Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Learning Git with simple experiments! Add this line in VS folder project  and then pushed");
 Console.WriteLine("  Added this line in CommandLine project(via VS)   and then pushed");
+Console.WriteLine("  Added this line in CommandLine project(via VS)   to show the staging area");
